f6d19fffa468ee2c4e973f8ba0403dd74557679f
Ciprian Prohozescu <ciprian.prohozescu@internetzoo.dk>
2020-03-02 11:06:48 +0100

    Add database setup

--- ringdumpTrainer.py
+++ ringdumpTrainer.py
@@ -37,7 +37,8 @@ def purge():
 def setup():
     create_events = """CREATE TABLE IF NOT EXISTS events (
         id integer PRIMARY KEY AUTOINCREMENT,
-        name text NOT NULL,
+        event text NOT NULL,
+        node text NOT NULL,
         count integer NOT NULL);"""
     create_event_relations = """CREATE TABLE IF NOT EXISTS event_relations (
         cause integer,
@@ -49,31 +50,6 @@ def setup():
     execute_statement(create_events)
     execute_statement(create_event_relations)
 
-def insert_mock_data():
-    insert_event1 = """INSERT INTO events (name, count)
-        VALUES ('node down', 3);"""
-    insert_event2 = """INSERT INTO events (name, count)
-        VALUES ('interface down', 5);"""
-    insert_event_relations = """INSERT INTO event_relations (cause, effect, count, time_average)
-        VALUES (1, 2, 2, 2350);"""
-
-    execute_statement(insert_event1)
-    execute_statement(insert_event2)
-    execute_statement(insert_event_relations)
-
 create_connection(r'../mecs.db')
 purge()
 setup()
-insert_mock_data()
-
-select_events = """SELECT * FROM events;"""
-select_event_relations = """SELECT * FROM event_relations;"""
-
-events = execute_query(select_events)
-event_relations = execute_query(select_event_relations)
-
-for event in events:
-    print(event)
-
-for event_relation in event_relations:
-    print(event_relation)
