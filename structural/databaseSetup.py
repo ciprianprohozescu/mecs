ac0fcb6e0b3eb48339a92f2206f97803cd48e4fa
Ciprian Prohozescu <ciprian.prohozescu@internetzoo.dk>
2020-03-11 12:51:00 +0100

    Add "structural" setup scripts

---
+++ structural/databaseSetup.py
@@ -0,0 +1,30 @@
+from databaseHelper import DBConnection
+
+db_file = r'../../mecs.db'
+connection = DBConnection(db_file)
+
+def purge():
+    drop_properties = 'DROP TABLE IF EXISTS properties;'
+    drop_property_relations = 'DROP TABLE IF EXISTS property_relations'
+
+    connection.execute_statement(drop_properties)
+    connection.execute_statement(drop_property_relations)
+
+def setup():
+    create_properties = """CREATE TABLE IF NOT EXISTS properties (
+        type text NOT NULL,
+        value text NOT NULL,
+        relation_count integer NOT NULL,
+        PRIMARY KEY (type, value));"""
+    create_property_relations = """CREATE TABLE IF NOT EXISTS property_relations (
+        cause integer,
+        effect integer,
+        count integer NOT NULL,
+        time_average integer NOT NULL,
+        PRIMARY KEY (cause, effect));"""
+    
+    connection.execute_statement(create_properties)
+    connection.execute_statement(create_property_relations)
+
+purge()
+setup()
