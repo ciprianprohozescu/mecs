e4aa7549594360ce8758dacd1964eab43983da61
Ciprian Prohozescu <ciprian.prohozescu@internetzoo.dk>
2020-03-19 15:38:34 +0100

    Add ringdump source

---
+++ sources/ringdumpEvents.py
@@ -0,0 +1,53 @@
+import pika
+import os
+import json
+import time
+
+#RabbitMQ setup
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+channel.exchange_declare(exchange='events', exchange_type='direct')
+channel.queue_declare(queue='in.ringdump')
+channel.queue_bind(exchange='events', queue='in.ringdump')
+
+#open ringdump
+ringdump = open(os.path.expanduser('~/Telenor/ringdump.json'))
+print(" Sending events... press CTRL+C to terminate")
+
+#read the first event to set the initial time
+line = ringdump.readline()
+event = json.loads(line)
+while not 'time' in event:
+    line = ringdump.readline()
+    event = json.loads(line)
+
+last_event_theoretical = float(event['time'])
+channel.basic_publish(exchange='events', routing_key='in.ringdump', body=line)
+print(" [x] Sent event!")
+last_event_actual = float(time.time())
+
+#simulate event dispatch using their timestamps
+try:
+    while True:
+        line = ringdump.readline()
+        event = json.loads(line)
+        while not 'time' in event:
+            line = ringdump.readline()
+            event = json.loads(line)
+        
+        try:
+            while (float(event['time']) - last_event_theoretical) > 1000 * (time.time() - last_event_actual):
+                continue
+        except KeyboardInterrupt:
+            break
+
+        last_event_theoretical = float(event['time'])
+        channel.basic_publish(exchange='events', routing_key='in.ringdump', body=line)
+        print(" [x] Sent event!")
+        last_event_actual = time.time()
+except KeyboardInterrupt:
+    pass
+
+print(" Closing connection...")
+
+connection.close()
