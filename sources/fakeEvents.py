43aeea71acf2a3ec82746d178e676f008e28ae64
MII <mnel.jnr@gmail.com>
2020-04-10 16:35:51 +0200

    modified gateway and fake-events for docker

--- sources/fakeEvents.py
+++ sources/fakeEvents.py
@@ -3,41 +3,38 @@ import os
 import json
 import time
 
-#RabbitMQ setup
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
-channel.exchange_declare(exchange = 'events-in', exchange_type = 'direct')
-channel.queue_declare(queue = 'in.fake', auto_delete = True)
-channel.queue_bind(exchange = 'events-in', queue = 'in.fake', routing_key = 'in.fake')
+class Fake_Event_Data_Gen:
+    def __init__(self, file_loc):
+        #RabbitMQ setup
+        amqp_url = os.environ['AMQP_URL']
+        print('URL: %s' % (amqp_url,))
+        parameters = pika.URLParameters(amqp_url)
+        connection = pika.BlockingConnection(parameters)
 
-#open the fake dump
-fakedump = open(os.path.expanduser('~/Telenor/fake_data.json'))
-print(" Sending events... press CTRL+C to terminate")
+        self.channel = connection.channel()
+        self.channel.exchange_declare(exchange = 'events-in', exchange_type = 'direct')
+        self.channel.queue_declare(queue = 'in.fake', auto_delete = True)
+        self.channel.queue_bind(exchange = 'events-in', queue = 'in.fake', routing_key = 'in.fake')
 
-#assign a timestamp to the first event
-last_event = time.time()
+        self.file_loc = file_loc
 
-#simulate event dispatch using their time_difs
-try:
-    while True:
+    def message_loop(self):
+        """simulate event dispatch using their time_difs"""
+        fakedump = open(self.file_loc, 'r')
         line = fakedump.readline()
-        event = json.loads(line)
-        
-        try:
-            while event['time_dif'] > 1000 * (time.time() - last_event):
-                continue
-        except KeyboardInterrupt:
-            break
+        print(" Sending events... press CTRL+C to terminate")
+        # just in case, don't want any unexpected invalid data errors
+        while line:
+            event = json.loads(line)
+            
+            time_diff =  event.pop('time_dif')
+            event['time'] = time.time()
+            
+            self.channel.basic_publish(exchange = 'events-in', routing_key = 'in.fake', body = json.dumps(event))
+            print(" [x] Sent event!")
+            time.sleep(time_diff/1000)
+            line = fakedump.readline()
 
-        del event['time_dif']
-        event['time'] = time.time()
-        
-        channel.basic_publish(exchange = 'events-in', routing_key = 'in.fake', body = json.dumps(event))
-        print(" [x] Sent event!")
-        last_event = time.time()
-except KeyboardInterrupt:
-    pass
-
-print(" Closing connection...")
-
-connection.close()
+if __name__ == "__main__":
+    fake_event_data_gen = Fake_Event_Data_Gen('fake_data.json')
+    fake_event_data_gen.message_loop()
