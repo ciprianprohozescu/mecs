6124cfbbd838d5641a9f1ac0f3be3cfbc0f90aeb
Ciprian Prohozescu <ciprian.prohozescu@internetzoo.dk>
2020-05-06 10:38:16 +0200

    Connect to webApi regardless of host

--- web-mecs/src/components/EventsList.vue
+++ web-mecs/src/components/EventsList.vue
@@ -49,7 +49,7 @@ export default {
 
   methods: {
     getData() {
-      axios.get('http://localhost:5000/').then(response => {
+      axios.get('http://' + location.hostname + ':5000/').then(response => {
         this.events = response.data.reverse();
 
         for (let i = 0; i < this.events.length; i++) {
