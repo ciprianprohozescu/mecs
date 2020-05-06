e981c23553f866e557f7d1b8eb785e49888ede5a
Ciprian Prohozescu <ciprian.prohozescu@internetzoo.dk>
2020-05-06 13:38:28 +0200

    Also update the API endpoint in EventsList

--- web-mecs/src/components/EventsList.vue
+++ web-mecs/src/components/EventsList.vue
@@ -49,7 +49,7 @@ export default {
 
   methods: {
     getData() {
-      axios.get('http://localhost:5000/').then(response => {
+      axios.get(process.env.VUE_APP_EVENTS_ENDPOINT).then(response => {
         this.events = response.data.reverse();
 
         for (let i = 0; i < this.events.length; i++) {
