a81ebc003a7722df092647696120602f7b631a4e
MII <mnel.jnr@gmail.com>
2020-05-17 01:10:44 +0200

    cleaned up sources, they now look for a file in the cwd unless otherwise specified... put the keyboard interrupt back in

--- sources/fake_data/fakeEvents.py
+++ sources/fake_data/fakeEvents.py
@@ -1,5 +1,15 @@
+"""
+Simulates sending kibana events to the queue
+Run conditions:
+    python3 fakeEvents.py
+    python3 fakeEvents.py <path/to/fake_data.json>
+if no path is specified, assumes it is in the cwd
+"""
+
+
 import pika
 import os
+import sys
 import json
 import time
 
@@ -24,17 +34,23 @@ class Fake_Event_Data_Gen:
         print(" Sending events... press CTRL+C to terminate")
         # just in case, define an endpoint don't want any unexpected invalid data errors
         while line:
-            event = json.loads(line)
-            
-            time_diff =  event.pop('time_dif')
-            event['time'] = time.time()
-            
-            self.channel.basic_publish(exchange = 'events-in', routing_key = 'in.fake', body = json.dumps(event))
-            print(" [x] Sent event!")
-            time.sleep(time_diff/1000)
-            line = fakedump.readline()
+            try:
+                event = json.loads(line)
+                
+                time_diff =  event.pop('time_dif')
+                event['time'] = time.time()
+                
+                self.channel.basic_publish(exchange = 'events-in', routing_key = 'in.fake', body = json.dumps(event))
+                print(" [x] Sent event!")
+                time.sleep(time_diff/1000)
+                line = fakedump.readline()
+            except KeyboardInterrupt:
+                self.connection.close()
+                print(" Connection closed")
+                break
 
 if __name__ == "__main__":
     amqp_url = os.environ['AMQP_URL'] if 'AMQP_URL' in os.environ else 'http://localhost'
-    fake_event_data_gen = Fake_Event_Data_Gen(amqp_url, 'fake_data.json')
+    file_loc = sys.argv[1] if len(sys.argv) > 1 else "fake_data.json"
+    fake_event_data_gen = Fake_Event_Data_Gen(amqp_url, file_loc)
     fake_event_data_gen.message_loop()
--- sources/ringdump/ringdumpEvents.py
+++ sources/ringdump/ringdumpEvents.py
@@ -1,5 +1,14 @@
+"""
+Simulates sending ring events to the queue
+Run conditions:
+    python3 ringdumpEvents.py
+    python3 ringdumpEvents.py <path/to/ringdump.json>
+if no path is specified, assumes it is in the cwd
+"""
+
 import pika
 import os
+import sys
 import json
 import time
 
@@ -8,9 +17,9 @@ class RingDumpEvents:
         #RabbitMQ setup
         print('URL: %s' % (amqp_url,))
         parameters = pika.URLParameters(amqp_url)
-        connection = pika.BlockingConnection(parameters)
+        self.connection = pika.BlockingConnection(parameters)
 
-        self.channel = connection.channel()
+        self.channel = self.connection.channel()
         self.channel.exchange_declare(exchange = 'events-in', exchange_type = 'direct')
         self.channel.queue_declare(queue = 'in.ringdump', auto_delete = True)
         self.channel.queue_bind(exchange = 'events-in', queue = 'in.ringdump', routing_key = 'in.ringdump')
@@ -23,23 +32,32 @@ class RingDumpEvents:
         print(" Sending events... press CTRL+C to terminate")
         ringdump = open(self.file_loc, 'r')
         last_event = 0
+        line = "placeholder"
 
         # read the file in reverse order
-        for line in reversed(ringdump.readlines()):
-            event = json.loads(line)
+        while line:
+            try:
+                line = ringdump.readline()
+                event = json.loads(line)
+                if 'time' not in event:
+                    # not all events have a timestamp, ignore them
+                    continue
 
-            # wait until next event is set to run, limit to 1 second to prevent elongated quiet periods
-            limiter = lambda x, y: (x-y if (x-y < 1) else 1)
-            time.sleep(limiter(event['time'], last_event))
+                # wait until next event is set to run, limit to 1 second to prevent elongated quiet periods
+                limiter = lambda x, y: (x-y if (x-y < 1) else 1)
+                time.sleep(limiter(event['time'], last_event))
 
-            self.channel.basic_publish(exchange = 'events-in', routing_key = 'in.ringdump', body = line)
-            print(" [x] Sent event!")
+                self.channel.basic_publish(exchange = 'events-in', routing_key = 'in.ringdump', body = line)
+                print(" [x] Sent event!")
 
-            last_event = event['time']
-
-        print(" Closing connection...")
+                last_event = event['time']
+            except KeyboardInterrupt:
+                self.connection.close()
+                print(" Closing connection...")
+                break
 
 if __name__ == "__main__":
     amqp_url = os.environ['AMQP_URL'] if 'AMQP_URL' in os.environ else 'http://localhost'
-    ringdump_events = RingDumpEvents(amqp_url, 'ringdump.json')
+    file_loc = sys.argv[1] if len(sys.argv) > 1 else "ringdump.json"
+    ringdump_events = RingDumpEvents(amqp_url, file_loc)
     ringdump_events.message_loop()
